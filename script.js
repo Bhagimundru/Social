6c238bbe6f9172e4d8d39bcacaa5582e09ed6aef
Bhagya Sri <bhagimundru@gmail.com>
2023-06-22 00:54:09 +0530

    first push

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+type="text/javascript">
+  (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
+      (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
+    var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
+    var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-EOXVMFT8UBRB-2");
+
 function allowuser(form) {
     let a = document.getElementById("user").value;
     var b="";
     b= a.substr(8,5);
     var id = b;
     //passing user and account objects:
-    /*aptrinsic("identify",
+    aptrinsic("identify",
         {
             //User Fields
             "id": b, // Required for logged in app users
@@ -15,7 +22,7 @@ function allowuser(form) {
         {
             //Account Fields
             "id": "Account6", //Required
-        });*/
+        });
         alert("Valid User");
         form.action = "Landingpage.html";
         alert("Logged in user id :"+b);
