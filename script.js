53a4ab3014fe6322511b286492371c68f1a476e6
Bhagya Sri <bhagimundru@gmail.com>
2024-01-08 19:05:56 +0530

    iframe

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ window.rudderAnalyticsMount()}window.rudderanalytics.load("2Xx5eab4YxSb0TMDi6UDQ
       (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-MTUFRPVCOSHV-2",,{ "iframeModeEnabled": false });
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-MTUFRPVCOSHV-2",{ "iframeModeEnabled": false });
 
 function allowuser(form) {
     let a = document.getElementById("user").value;
